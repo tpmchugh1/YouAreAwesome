21c52039a2b2290f185716930e1e0c56c132d443
Terrance McHugh <tpmchugh@mac.com>
2026-02-01 18:10:24 -0500

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,21 @@
+//  ContentView.swift
+//  YouAreAwesome
+//  ©Created by Terrance McHugh on 2/1/26
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,14 @@
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//  ©Created by Terrance McHugh on 2/1/26
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
